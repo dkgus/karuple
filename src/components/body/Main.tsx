6516f03268e65b81f0d77eb2719d03ea0b839e03
dkgus <dkgus12dkgus@naver.com>
2024-02-21 15:37:39 +0900

    feat: 폰트 추가+ 메인페이지 퍼블리싱

--- src/components/body/Main.tsx
+++ src/components/body/Main.tsx
@@ -1,14 +1,29 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Main = (): JSX.Element => {
+  const navigate = useNavigate();
+
   return (
-    <div className="main-wrapper">
-      <img
-        className="main-image"
-        src="https://lh3.google.com/u/0/d/1o8zsXyxurQsndrHytpDw6tpY2dHIFuyf=w2000-h3556-iv1"
-        alt="Main"
-        style={{ height: "100vh", width: "100%" }}
-      />
+    <div
+      className="text-3xl text-center text-white pt-10 relative flex flex-col justify-center items-center"
+      style={{
+        backgroundImage: `url("https://lh3.google.com/u/0/d/1o8zsXyxurQsndrHytpDw6tpY2dHIFuyf=w2000-h3556-iv1")`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        height: "100vh",
+        width: "100%",
+      }}
+    >
+      <div className="mb-auto">카러플 Mbti</div>
+      <button
+        className="btn btn-warning absolute bottom-0 mb-10"
+        onClick={() => {
+          navigate("/test");
+        }}
+      >
+        시작하기
+      </button>
     </div>
   );
 };
