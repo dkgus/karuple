e50de3956a26c63cdbecae5954a454767fc5b24e
dkgus <dkgus12dkgus@naver.com>
2024-02-21 17:32:18 +0900

    feat: 메인 페이지 디자인 상세

--- src/components/body/Main.tsx
+++ src/components/body/Main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import img from "./img/main_img.jpg";
 
 const Main = (): JSX.Element => {
   const navigate = useNavigate();
@@ -8,21 +9,34 @@ const Main = (): JSX.Element => {
     <div
       className="text-3xl text-center text-white pt-10 relative flex flex-col justify-center items-center"
       style={{
-        backgroundImage: `url("https://lh3.google.com/u/0/d/1o8zsXyxurQsndrHytpDw6tpY2dHIFuyf=w2000-h3556-iv1")`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
         height: "100vh",
         width: "100%",
+        overflow: "hidden",
       }}
     >
-      <div className="mb-auto">카러플 Mbti</div>
+      <img
+        src={img}
+        //src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg"
+        //https://lh3.google.com/u/0/d/1o8zsXyxurQsndrHytpDw6tpY2dHIFuyf=w2000-h3556-iv1
+        alt="배경 이미지"
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          objectFit: "cover",
+        }}
+      />
+      <div className="mb-auto z-50 text-4xl">카러플 Mbti</div>
       <button
-        className="btn btn-warning absolute bottom-0 mb-10"
+        style={{ height: "30px" }}
+        className="btn btn-sm btn-warning absolute bottom-0 mb-10 text-2xl font-light"
         onClick={() => {
           navigate("/test");
         }}
       >
-        시작하기
+        START
       </button>
     </div>
   );
