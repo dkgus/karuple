a0aad68145ce90a8425b27b23f6c7f75df94f2da
dkgus <dkgus12dkgus@naver.com>
2024-02-21 14:45:03 +0900

    feat: 디바이스별 화면 풀사이즈 크기조정

--- src/components/body/Main.tsx
+++ src/components/body/Main.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+
 const Main = (): JSX.Element => {
   return (
-    <div>
-      123
-      {/* <img src="https://lh3.google.com/u/0/d/1o8zsXyxurQsndrHytpDw6tpY2dHIFuyf=w2000-h3556-iv1"></img> */}
+    <div className="main-wrapper">
+      <img
+        className="main-image"
+        src="https://lh3.google.com/u/0/d/1o8zsXyxurQsndrHytpDw6tpY2dHIFuyf=w2000-h3556-iv1"
+        alt="Main"
+        style={{ height: "100vh", width: "100%" }}
+      />
     </div>
   );
 };
