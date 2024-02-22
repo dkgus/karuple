86f4fbf1c2cdb25cd78f70f196fc51d06c0b55d6
dkgus <dkgus12dkgus@naver.com>
2024-02-22 17:55:58 +0900

    feat: 타입별 계산 알고리즘 추가

--- src/components/asset/data.js
+++ src/components/asset/data.js
@@ -3,12 +3,92 @@ const data = [
     id: 1,
     question:
       "랭킹전을 하고 있는 당신, 만약 앞사람이 동굴앞에서 막기를 한다면? ",
-    answer: ["답변1", "답변2", "답변3"],
+    type: "first",
+    answers: [
+      {
+        text: "답변1",
+        point: 1,
+      },
+      {
+        text: "답변2",
+        point: -1,
+      },
+    ],
   },
   {
     id: 2,
     question: "아깝게 랭킹전에서 진 상황에서 당신은 어떻게 할 것인가요?  ",
-    answer: ["답변1", "답변2", "답변3"],
+    type: "first",
+    answers: [
+      {
+        text: "답변1",
+        point: 1,
+      },
+      {
+        text: "답변2",
+        point: -1,
+      },
+    ],
+  },
+  {
+    id: 3,
+    question: "아깝게 랭킹전에서 진 상황에서 당신은 어떻게 할 것인가요?  ",
+    type: "first",
+    answers: [
+      {
+        text: "답변1",
+        point: 1,
+      },
+      {
+        text: "답변2",
+        point: -1,
+      },
+    ],
+  },
+  {
+    id: 4,
+    question: "아깝게 랭킹전에서 진 상황에서 당신은 어떻게 할 것인가요?  ",
+    type: "second",
+    answers: [
+      {
+        text: "답변1",
+        point: 1,
+      },
+      {
+        text: "답변2",
+        point: -1,
+      },
+    ],
+  },
+  {
+    id: 5,
+    question: "아깝게 랭킹전에서 진 상황에서 당신은 어떻게 할 것인가요?  ",
+    type: "second",
+    answers: [
+      {
+        text: "답변1",
+        point: 1,
+      },
+      {
+        text: "답변2",
+        point: -1,
+      },
+    ],
+  },
+  {
+    id: 6,
+    question: "아깝게 랭킹전에서 진 상황에서 당신은 어떻게 할 것인가요?  ",
+    type: "second",
+    answers: [
+      {
+        text: "답변1",
+        point: 1,
+      },
+      {
+        text: "답변2",
+        point: -1,
+      },
+    ],
   },
 ];
 
--- src/components/body/Test.tsx
+++ src/components/body/Test.tsx
@@ -1,6 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import data from "../asset/data";
 import { useNavigate } from "react-router-dom";
+
+interface Answer {
+  text: string;
+  point: number;
+}
+
+interface Question {
+  id: number;
+  question: string;
+  type: string;
+  answers: Answer[];
+}
+
+interface AnswerSheet {
+  type: string;
+  score: number;
+  result: string;
+}
 
 const Test = (): JSX.Element => {
   const style = {
@@ -12,6 +30,43 @@ const Test = (): JSX.Element => {
 
   const [idx, setIndex] = useState<number>(0);
   const [loading, setLoading] = useState<boolean>(false);
+  const [answerSheet, setAnswerSheet] = useState<AnswerSheet[]>([]);
+
+  useEffect(() => {
+    const initializeAnswerSheet = () => {
+      const arr: AnswerSheet[] = [];
+      const types: string[] = ["first", "second", "third", "fourth"];
+      for (let i = 0; i < 4; i++) {
+        arr.push({
+          type: types[i],
+          score: 0,
+          result: "",
+        });
+      }
+      setAnswerSheet(arr);
+    };
+
+    initializeAnswerSheet();
+  }, []);
+
+  const handleAnswerClick = (point: number, type: string) => {
+    setAnswerSheet((prevSheet) =>
+      prevSheet.map((sheet) =>
+        sheet.type === type ? { ...sheet, score: sheet.score + point } : sheet
+      )
+    );
+
+    if (idx + 1 !== data.length) {
+      setIndex((prevIdx) => prevIdx + 1);
+    } else {
+      setLoading(true);
+      setTimeout(() => {
+        navigate("/result/1");
+      }, 3000);
+    }
+  };
+
+  const currentQuestion: Question = data[idx];
 
   return (
     <div>
@@ -30,24 +85,20 @@ const Test = (): JSX.Element => {
       ) : (
         <>
           <div className={style.class + " bg-base-100"} style={style}>
-            Q{data[idx].id}. {data && data[idx].question}
+            Q{currentQuestion.id}. {currentQuestion.question}
           </div>
-          <div
-            className={style.class + " bg-base-100/50"}
-            style={style}
-            onClick={() => {
-              if (idx + 1 !== data.length) {
-                setIndex((prev) => prev + 1);
-              } else {
-                setLoading(true);
-                setTimeout(() => {
-                  navigate("/result/1");
-                }, 3000);
+          {currentQuestion.answers.map((answer, index) => (
+            <div
+              key={index}
+              className={style.class + " bg-base-100/50 active:bg-base-100"}
+              style={style}
+              onClick={() =>
+                handleAnswerClick(answer.point, currentQuestion.type)
               }
-            }}
-          >
-            답변
-          </div>
+            >
+              {answer.text}
+            </div>
+          ))}
         </>
       )}
     </div>
