1be7966726a6f8baf08781d25f9f5f1626f8dfbf
dkgus <dkgus12dkgus@naver.com>
2024-02-22 15:33:19 +0900

    feat: 클릭 시 다음 문제 이동 처리 + 평가 후 로딩페이지 퍼블리싱

--- src/components/body/Test.tsx
+++ src/components/body/Test.tsx
@@ -1,25 +1,55 @@
 import React, { useState } from "react";
 import data from "../asset/data";
+import { useNavigate } from "react-router-dom";
 
 const Test = (): JSX.Element => {
-  const commonClass = "card mx-auto mt-7 rounded-lg";
-
-  const comonStyle = {
+  const style = {
     width: "80%",
     fontSize: "20px",
+    class: "card mx-auto mt-7 rounded-lg",
   };
+  const navigate = useNavigate();
+
+  const [idx, setIndex] = useState<number>(0);
+  const [loading, setLoading] = useState<boolean>(false);
 
   return (
     <div>
-      <div className={commonClass + " bg-base-100"} style={{ ...comonStyle }}>
-        Q1. 질문 질문
-      </div>
-      <div
-        className={commonClass + " bg-base-100/50"}
-        style={{ ...comonStyle }}
-      >
-        답변
-      </div>
+      {loading ? (
+        <div
+          style={{
+            height: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <span className="loading loading-dots loading-md absolute top-1/3"></span>
+          <div className="absolute top-1/2">결과를 산출중입니다.</div>
+        </div>
+      ) : (
+        <>
+          <div className={style.class + " bg-base-100"} style={style}>
+            Q{data[idx].id}. {data && data[idx].question}
+          </div>
+          <div
+            className={style.class + " bg-base-100/50"}
+            style={style}
+            onClick={() => {
+              if (idx + 1 !== data.length) {
+                setIndex((prev) => prev + 1);
+              } else {
+                setLoading(true);
+                setTimeout(() => {
+                  navigate("/result/1");
+                }, 3000);
+              }
+            }}
+          >
+            답변
+          </div>
+        </>
+      )}
     </div>
   );
 };
