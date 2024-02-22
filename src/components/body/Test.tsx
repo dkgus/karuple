d34c91b98883905cf92d29a67d3b5286a5638ce1
dkgus <dkgus12dkgus@naver.com>
2024-02-22 14:22:53 +0900

    feat: 테스트 페이지 레이아웃 설정

--- src/components/body/Test.tsx
+++ src/components/body/Test.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import data from "../asset/data";
 
 const Test = (): JSX.Element => {
-  return <div>Test</div>;
+  const commonClass = "card mx-auto mt-7 rounded-lg";
+
+  const comonStyle = {
+    width: "80%",
+    fontSize: "20px",
+  };
+
+  return (
+    <div>
+      <div className={commonClass + " bg-base-100"} style={{ ...comonStyle }}>
+        Q1. 질문 질문
+      </div>
+      <div
+        className={commonClass + " bg-base-100/50"}
+        style={{ ...comonStyle }}
+      >
+        답변
+      </div>
+    </div>
+  );
 };
 
 export default Test;
