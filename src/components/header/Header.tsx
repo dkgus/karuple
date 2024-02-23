764f946b154c1b6009940a9593bed697d20c8fad
dkgus <dkgus12dkgus@naver.com>
2024-02-23 10:50:04 +0900

    feat: 로딩 바 밑 이미지 애니메이션 처리

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,13 +1,38 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useLocation } from "react-router-dom";
+import Cart from "../body/img/kart.png";
+
 interface Props {
   index: number;
   loading: boolean;
 }
+
 const Header = (props: Props): JSX.Element => {
   const { index, loading } = props;
   const location = useLocation();
   const maxIndex = 12;
+
+  const imageRef = useRef<HTMLImageElement>(null);
+  const [currentIndex, setCurrentIndex] = useState(index);
+
+  useEffect(() => {
+    const moveImage = () => {
+      if (imageRef.current) {
+        imageRef.current.style.transition = "transform 0.3s linear";
+        imageRef.current.style.transform = `translateX(${24 * currentIndex}px)`;
+      }
+    };
+
+    const intervalId = setInterval(moveImage, 10);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [currentIndex]);
+
+  useEffect(() => {
+    setCurrentIndex(index);
+  }, [index]);
 
   return location.pathname.includes("/test") ? (
     loading ? null : (
@@ -17,6 +42,12 @@ const Header = (props: Props): JSX.Element => {
           value={index}
           max={maxIndex}
         ></progress>
+        <img
+          className="kart_img realative left-3"
+          src={Cart}
+          width={50}
+          ref={imageRef}
+        />
       </header>
     )
   ) : location.pathname.includes("/result") ? (
