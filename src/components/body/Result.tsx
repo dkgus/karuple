3e1cce09b8516ffe330399eaf48773ee7218feeb
dkgus <dkgus12dkgus@naver.com>
2024-02-27 11:01:37 +0900

    feat: 컬러 태그 랜덤 색상 부여+ 매칭 이미지 출력

--- src/components/body/Result.tsx
+++ src/components/body/Result.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import Dao from "./img/dao.jpg";
+import Desyni from "./img/desyni.jpg";
+import Baejji from "./img/baejji.jpg";
+import Eddi from "./img/eddi.jpg";
+import Marid from "./img/marid.jpg";
+import Sugar from "./img/sugar.jpg";
+import Miz from "./img/miz.jpg";
+import Mos from "./img/mos.jpg";
+import Ilyeong from "./img/ilyeong.jpg";
+import Chenchen from "./img/chenchen.jpg";
+import Neo from "./img/neo.jpg";
+import Naiad from "./img/naiad.jpg";
+import Reune from "./img/reune.jpg";
+import Hellios from "./img/hellios.jpg";
+import Toreu from "./img/toreu.jpg";
 import Daisy from "./img/daisy.jpg";
+
 import "./style.css";
 
 interface Props {
@@ -12,28 +27,57 @@ const Result = (props: Props): JSX.Element => {
   const { setIndexChecker } = props;
   const navigate = useNavigate();
   const { id } = useParams();
-  console.log("id ", id);
+  const [colorLine, setColorLine] = useState<string[]>([]);
 
-  const matchType = {
-    ESFP: "다오",
-    INTP: "디지니",
-    ISFP: "배찌",
-    ISTJ: "에띠",
-    INFP: "마리드",
-    ENTJ: "슈가",
-    ESTJ: "미즈 로두마니",
-    ISTP: "모스",
-    ESTP: "일영",
-    ENFP: "첸첸",
-    ESFJ: "네오",
-    INFJ: "나이아드",
-    ISFJ: "르네",
-    INTJ: "헬리오스",
-    ENTP: "토르",
-    ENFJ: "데이지",
+  const matchType: Record<
+    string,
+    { image: string; name: string; tag: string[] }
+  > = {
+    ESFP: { name: "다오", image: Dao, tag: ["열혈", "밝음", "낙관"] },
+    INTP: { name: "디지니", image: Desyni, tag: ["부끄럼", "과감"] },
+    ISFP: { name: "배찌", image: Baejji, tag: ["단순", "낙관", "게으름"] },
+    ISTJ: { name: "에띠", image: Eddi, tag: ["똑똑함", "박학다식", "설계"] },
+    INFP: {
+      name: "마리드",
+      image: Marid,
+      tag: ["도도", "낭만", "경쟁심없음", "사랑"],
+    },
+    ENTJ: { name: "슈가", image: Sugar, tag: ["열정", "승리"] },
+    ESTJ: {
+      name: "미즈 로두마니",
+      image: Miz,
+      tag: ["리더", "뛰어난 기술", "고집"],
+    },
+    ISTP: { name: "모스", image: Mos, tag: ["튜닝 마스터", "손기술", "성실"] },
+    ESTP: { name: "일영", image: Ilyeong, tag: ["대범", "거침없음", "명랑"] },
+    ENFP: { name: "첸첸", image: Chenchen, tag: ["활발", "친절", "활동적"] },
+    ESFJ: { name: "네오", image: Neo, tag: ["정의", "노력"] },
+    INFJ: { name: "나이아드", image: Naiad, tag: ["내성적", "희망"] },
+    ISFJ: { name: "르네", image: Reune, tag: ["내성적", "친구 좋아"] },
+    INTJ: { name: "헬리오스", image: Hellios, tag: ["신비주의"] },
+    ENTP: { name: "토르", image: Toreu, tag: ["용감", "자신감", "호쾌", "힘"] },
+    ENFJ: { name: "데이지", image: Daisy, tag: ["친절", "관리자", "열정"] },
   };
 
   const btnClass = "text-sm bg-base-100/50 p-2 rounded ";
+
+  useEffect(() => {
+    const colors: string[] = [
+      "#FFF7F1",
+      "#FFE4C9",
+      "#E78895",
+      "#BED1CF",
+      "#B784B7",
+      "#E493B3",
+      "#EEA5A6",
+    ];
+
+    const newBadgeColors = Array.from(
+      { length: matchType[id].tag.length },
+      () => colors[Math.floor(Math.random() * colors.length)]
+    );
+    setColorLine(newBadgeColors);
+  }, []);
 
   return (
     <div
@@ -49,10 +93,14 @@ const Result = (props: Props): JSX.Element => {
         className="img_container flex-1"
         style={{ overflowY: "auto", margin: "0 15px 0px 15px" }}
       >
-        <img src={Dao} className="mask mask-circle mx-auto h-full " alt="Dao" />
+        <img
+          src={matchType[id].image}
+          className="mask mask-circle mx-auto h-full "
+          alt="Dao"
+        />
       </div>
       <div className="text_container flex-1">
-        <div>{matchType[id]}</div>
+        <div>{matchType[id].name}</div>
         <div
           className="bg-base-100/50 mx-10 rounded custom_scroll text-base"
           style={{
@@ -62,19 +110,31 @@ const Result = (props: Props): JSX.Element => {
             maxHeight: "48vh",
           }}
         >
-          다오 성격 스크롤 테스트 다오 성격 스크롤 테스트 다오 성격 스크롤
-          테스트 다오 성격 스크롤 테스트 다오 성격 스크롤 테스트 다오 성격
-          스크롤 테스트 다오 성격 스크롤 테스트 다오 성격 스크롤 테스트 다오
-          성격 스크롤 테스트 다오 성격 스크롤 테스트 다오 성격 스크롤 테스트
-          다오 성격 스크롤 테스트 다오 성격 스크롤 테스트 다오 성격 스크롤
-          테스트 다오 성격 스크롤 테스트 다오 성격 스크롤 테스트 다오 성격
-          스크롤 테스트 다오 성격 스크롤 테스트 다오 성격 스크롤 테스트다오 성격
-          스크롤 테스트 다오 성격 스크롤 테스트 다오 성격 스크롤 테스트 다오
-          성격 스크롤 테스트 다오 성격 스크롤 테스트 다오 성격 스크롤 테스트
-          다오 성격 스크롤 테스트 다오 성격 스크롤 테스트 다오 성격 스크롤
-          테스트 다오 성격 스크롤 테스트 다오 성격 스크롤 테스트 다오 성격
-          스크롤 테스트 다오 성격 스크롤 테스트 다오 성격 스크롤 테스트 다오
-          성격 스크롤 테스트 다오 성격 스크롤 테스트
+          {matchType[id].tag.map((item, idx) => (
+            <div
+              className={`badge border-0 ml-1 rounded`}
+              key={item}
+              style={{ backgroundColor: colorLine[idx] }}
+            >
+              {item}
+            </div>
+          ))}
+
+          <div className="text-sm">
+            다오 성격 스크롤 테스트 다오 성격 스크롤 테스트 다오 성격 스크롤
+            테스트 다오 성격 스크롤 테스트 다오 성격 스크롤 테스트 다오 성격
+            스크롤 테스트 다오 성격 스크롤 테스트 다오 성격 스크롤 테스트 다오
+            성격 스크롤 테스트 다오 성격 스크롤 테스트 다오 성격 스크롤 테스트
+            다오 성격 스크롤 테스트 다오 성격 스크롤 테스트 다오 성격 스크롤
+            테스트 다오 성격 스크롤 테스트 다오 성격 스크롤 테스트 다오 성격
+            스크롤 테스트 다오 성격 스크롤 테스트 다오 성격 스크롤 테스트다오
+            성격 스크롤 테스트 다오 성격 스크롤 테스트 다오 성격 스크롤 테스트
+            다오 성격 스크롤 테스트 다오 성격 스크롤 테스트 다오 성격 스크롤
+            테스트 다오 성격 스크롤 테스트 다오 성격 스크롤 테스트 다오 성격
+            스크롤 테스트 다오 성격 스크롤 테스트 다오 성격 스크롤 테스트 다오
+            성격 스크롤 테스트 다오 성격 스크롤 테스트 다오 성격 스크롤 테스트
+            다오 성격 스크롤 테스트 다오 성격 스크롤 테스트
+          </div>
         </div>
       </div>
       <div className="btn_box absolute bottom-5 w-full">
